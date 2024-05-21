cc3469d6768d3eecb0f0327775cd26557e80712a
yook-jongho <dbrwhdgh1203@naver.com>
2024-05-21 18:59:13 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/Pages/LogIn/LogInPage.tsx
+++ src/Pages/LogIn/LogInPage.tsx
@@ -1,16 +1,27 @@
 export const LogInPage = () => {
     return (
         <div>
-            <span>매점의 눈</span>
-            <form>
-                <span>로그인</span>
-                <input type="email" />
-                <input type="password" />
-                <button> → </button>
-            </form>
-            <span>계정이 없으신가요? </span>
-            <span>회원가입</span>
-            <div className="text-3xl bg-blue-500">Tailwind Css 적용 테스트</div>
+            <section className="w-2/5 h-90 min-h-700 p-14 flex flex-col justify-between">
+                <span>매점의 눈</span>
+                <form className=" w-340 flex flex-col gap-3">
+                    <span className="mb-6 text-3xl font-semibold">로그인</span>
+                    <input
+                        type="email"
+                        placeholder="아이디를 입력하세요."
+                        className="py-2 pl-4 text-sm border border-solid border-black"
+                    />
+                    <input
+                        type="password"
+                        placeholder="비밀번호를 입력하세요."
+                        className="py-2 pl-4 text-sm border border-solid border-black"
+                    />
+                    <button className="h-10 bg-btn text-white"> → </button>
+                </form>
+                <div>
+                    <span>계정이 없으신가요? </span>
+                    <span className="text-skyblue underline">회원가입</span>
+                </div>
+            </section>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                340: "340px",
+            },
+            height: {
+                100: "100dvh",
+                90: "90dvh",
+            },
+            spacing: {
+                700: "700px",
+            },
+            colors: {
+                btn: "rgb(66, 66, 66)",
+                skyblue: "#00A3FF",
+            },
+        },
     },
     plugins: [],
 };
