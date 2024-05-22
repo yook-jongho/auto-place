4f29c1123cdb8fa2afe294fafcd0668dafdbb558
yook-jongho <dbrwhdgh1203@naver.com>
2024-05-23 03:10:49 +0900

    style: 커스텀 스타일 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
         extend: {
             width: {
                 340: "340px",
+                450: "450px",
+                480: "480px",
             },
             height: {
                 100: "100dvh",
                 90: "90dvh",
+                700: "700px",
             },
             spacing: {
                 700: "700px",
@@ -16,6 +19,7 @@ module.exports = {
             colors: {
                 btn: "rgb(66, 66, 66)",
                 skyblue: "#00A3FF",
+                input: "#DBDBDB",
             },
         },
     },
