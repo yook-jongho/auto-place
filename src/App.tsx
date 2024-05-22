1915f4bb7af2b63983a1f504b8d9a08615875635
yook-jongho <dbrwhdgh1203@naver.com>
2024-05-23 03:10:27 +0900

    feat: 회원가입 페이지, 메인 페이지 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import logo from "./logo.svg";
 import { LogInPage } from "./pages/LogIn/LogInPage";
+import { SignUpPage } from "./pages/SignUp/SignUpPage";
+import { MainPage } from "./pages/Main/MainPage";
 
 const Router = () => {
     return (
         <HashRouter>
             <Routes>
                 <Route path="/" element={<LogInPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/main" element={<MainPage />} />
             </Routes>
         </HashRouter>
     );
