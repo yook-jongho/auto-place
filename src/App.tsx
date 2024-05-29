be5a7c56d829371ff8ce2a60b82b0da08711aae5
yook-jongho <dbrwhdgh1203@naver.com>
2024-05-29 16:21:53 +0900

    feat: 페이지 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { LogInPage } from "./pages/LogIn/LogInPage";
 import { SignUpPage } from "./pages/SignUp/SignUpPage";
 import { MainPage } from "./pages/Main/MainPage";
+import { CctvPage } from "./pages/Setting/CCTV/CctvPage";
+import { GeneralPage } from "./pages/Setting/General/GeneralPage";
+import { AirconPage } from "./pages/Setting/Aircon/AirconPage";
+import { DoorlockPage } from "./pages/Setting/Doorlock/DoorlockPage";
+import { createHashHistory } from "history";
+
+const hashHistory = createHashHistory();
 
 const Router = () => {
     return (
@@ -11,6 +18,10 @@ const Router = () => {
                 <Route path="/" element={<LogInPage />} />
                 <Route path="/signup" element={<SignUpPage />} />
                 <Route path="/main" element={<MainPage />} />
+                <Route path="/general" element={<GeneralPage />} />
+                <Route path="/cctv" element={<CctvPage />} />
+                <Route path="/doorlock" element={<DoorlockPage />} />
+                <Route path="/aircon" element={<AirconPage />} />
             </Routes>
         </HashRouter>
     );
