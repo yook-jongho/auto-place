cbe80fa4225138689286938dd6380ac3267e6c10
yook-jongho <dbrwhdgh1203@naver.com>
2024-05-29 16:11:50 +0900

    feat: mainpage 퍼블리싱 및 네비게이트 기능 추가

--- src/Pages/Main/MainPage.tsx
+++ src/Pages/Main/MainPage.tsx
@@ -1,27 +1,62 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { useNavigate } from "react-router-dom";
+import { Header } from "../Setting/components/header";
 import gear from "../../assets/gear.png";
 import cctv from "../../assets/cctv.png";
 import aircon from "../../assets/aircon.png";
 import doorlock from "../../assets/doorlock.png";
+import { useEffect } from "react";
 
 export const MainPage = () => {
+    const navigator = useNavigate();
+    const movePage = (device: String) => {
+        navigator(`/${device}`);
+    };
+
+    useEffect(() => {
+        const checkAndCreateJsonFile = async () => {
+            try {
+                const response = await window.electron.checkAndCreateJson();
+                console.log(response);
+            } catch (error) {
+                console.error("Failed to check/create JSON file:", error);
+            }
+        };
+
+        checkAndCreateJsonFile();
+    }, []);
+
     return (
-        <div className="w-480 m-auto mt-24 h-100 text-center m-auto">
-            <span className="text-2xl font-semibold">시작하기</span>
-            <div className="grid grid-cols-2 mt-28 gap-52">
-                <div className="flex flex-col items-center">
-                    <img className="w-20 h-20" src={gear} />
+        <div className="flex flex-col gap-20 m-auto p-20 content-center items-center justify-center">
+            <Header name="시작하기" />
+            {/* <span className="text-2xl font-semibold">시작하기</span> */}
+            <div className="w-480 grid grid-cols-2 m-auto mt-28 gap-52">
+                <div
+                    className="flex flex-col items-center"
+                    onClick={() => movePage("general")}
+                >
+                    <img className="w-20 h-20 mb-16" src={gear} />
                     <span>일반</span>
                 </div>
-                <div className="flex flex-col items-center">
-                    <img className="w-20 h-20" src={cctv} />
+                <div
+                    className="flex flex-col items-center"
+                    onClick={() => movePage("cctv")}
+                >
+                    <img className="w-20 h-20 mb-16" src={cctv} />
                     <span>CCTV</span>
                 </div>
-                <div className="flex flex-col items-center">
-                    <img className="w-20 h-20" src={doorlock} />
+                <div
+                    className="flex flex-col items-center"
+                    onClick={() => movePage("doorlock")}
+                >
+                    <img className="w-20 h-20 mb-16" src={doorlock} />
                     <span>도어락</span>
                 </div>
-                <div className="flex flex-col items-center">
-                    <img className="w-20 h-20" src={aircon} />
+                <div
+                    className="flex flex-col items-center"
+                    onClick={() => movePage("aircon")}
+                >
+                    <img className="w-20 h-20 mb-16" src={aircon} />
                     <span>에어컨</span>
                 </div>
             </div>
