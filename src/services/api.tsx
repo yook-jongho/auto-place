325f1ca21d1cb64bdcdc9501f1b4b87275717c6f
yook-jongho <dbrwhdgh1203@naver.com>
2024-06-04 15:17:44 +0900

    feat: updateDeivce함수 비동기 처리

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -12,6 +12,23 @@ export const loadDevices = async () => {
 export const addDevices = async (newDevice: CCTV) => {
     try {
         const response = await window.electron.addCctvDevice("CCTV", newDevice);
+        console.log(response);
+    } catch (error) {
+        console.error("Failed to add CCTV device:", error);
+    }
+};
+
+export const updateDevices = async (
+    device: string,
+    newDevice: CCTV,
+    id: string
+) => {
+    try {
+        const response = await window.electron.updateDevice(
+            "CCTV",
+            id,
+            newDevice
+        );
         if (response.status === "success") console.log("성공");
     } catch (error) {
         console.error("Failed to add CCTV device:", error);
